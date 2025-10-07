107b6e545ccdefee7b9eef1a8a6a6973a8a4a9c6
EAS77896 <eiska1991@hvl.no>
2025-10-07 15:27:39 +0200

    Test1

--- src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
@@ -17,5 +17,29 @@ public class CustomerMain {
          Write code that creates a Customer object and teste the methods implemented in the class
 
         */
+
+        Customer mats = new Customer("Mads Dittman Mikkelsen","mats@gmail.com",1004, PowerAgreementType.SPOTPRICE);
+
+        System.out.println("**************TEST*************");
+
+
+        System.out.println("\n**********Get Metoder**********\n");
+
+        System.out.println("Navn: " + mats.getName());
+        System.out.println("Email: " + mats.getEmail());
+        System.out.println("id: " + mats.getCustomer_id());
+        System.out.println("Agreement: " + mats.getAgreement());
+
+        System.out.println("\n***********To String***********\n");
+
+        System.out.println(mats.toString());
+
+        System.out.println("\n**********Set Metoder**********\n");
+
+        mats.setName("Markus Mikkelsen");
+        mats.setCustomer_id(100);
+        mats.setEmail("markus@gmail.com");
+        mats.setAgreement(PowerAgreementType.NORGESPRICE);
+        System.out.print(mats.toString());
     }
 }
